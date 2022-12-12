f8cbf60b8e379927ddb2eb1870eeb17d192ce09f
Jake Carpenter <jake@jakecarpenter.dev>
2022-12-11 19:12:35 -0700

    Backfill day 1

---
+++ Days/Day1.cs
@@ -0,0 +1,38 @@
+namespace AdventOfCode.Days;
+
+public class Day1 : BaseDay
+{
+    protected override string DayNumberDisplay => "01";
+
+    protected override async Task<string> GetPartOne()
+    {
+        var calories = await BuildElvCaloriesCatalog();
+        return calories.Max().ToString();
+    }
+
+    protected override async Task<string> GetPartTwo()
+    {
+        var calories = await BuildElvCaloriesCatalog();
+        return calories.OrderByDescending(x => x).Take(3).Sum().ToString();
+    }
+
+    private static async Task<List<int>> BuildElvCaloriesCatalog()
+    {
+        var calories = new List<int>();
+        var currentElfCalories = 0;
+
+        await foreach (var line in Utilities.ReadFileLinesAsync(1))
+        {
+            if (line is not { Length: > 0 })
+            {
+                calories.Add(currentElfCalories);
+                currentElfCalories = 0;
+                continue;
+            }
+
+            currentElfCalories += int.Parse(line);
+        }
+
+        return calories;
+    }
+}
